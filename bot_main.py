9b89bbe0372c5c8ceb9235cceb268f3cf420a7e7
ogofen <origofen@gmail.com>
2020-07-11 17:05:48 +0300

    tradefile bug fix

--- bot_main.py
+++ bot_main.py
@@ -20,7 +20,6 @@ times  = sys.argv[3]
 stopPx = sys.argv[4]
 strategy = sys.argv[5]
 counter = 0
-tradeFile = 'scalp_{}_{}'.format(side, symbol)
 print("looking for {} Trades of {} {}".format(times, side, symbol))
 while (counter < int(times)):
     with open("{}{}".format(symbol, historicalFile)) as json_file:
@@ -41,6 +40,7 @@ while (counter < int(times)):
         json.dump(scalp_info, json_file)
     if data[-1]['close'] < float(band_low.iloc[-1]) and abs(data[-1]['close'] - float(band_low.iloc[-1])) >= closeInterval[symbol]:
         if side == "Buy" or side == "Both":
+            tradeFile = 'scalp_{}_{}'.format("Buy", symbol)
             if not os.path.exists(tradeFile):
                 os.system("php CreateTrade.php {} Buy {} {} {} &".format(symbol, amount, stopPx, strategy))
                 counter = counter + 1
@@ -48,6 +48,7 @@ while (counter < int(times)):
                 sleep(5)
     if data[-1]['close'] > float(band_high.iloc[-1]) and abs(float(band_high.iloc[-1]) - data[-1]['close']) >= closeInterval[symbol]:
         if side == "Sell" or side == "Both":
+            tradeFile = 'scalp_{}_{}'.format("Sell", symbol)
             if not os.path.exists(tradeFile):
                 os.system("php CreateTrade.php {} Sell {} {} {} &".format(symbol, amount, stopPx, strategy))
                 counter = counter + 1
